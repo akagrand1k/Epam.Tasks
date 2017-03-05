e26fb1c976068dead186e60c5c1a5513df8d6dc0
Maksim Leontev <akagrand1k@yandex.ru>
2017-03-05 22:55:46 +0300

    Task03

---
+++ Task03/Epam.3.3-DynamicArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam._3._3_DynamicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            arr[0] = 5;
+            arr[1] = 6;
+            arr[2] = 3;
+            DynamicArray<int> str = new DynamicArray<int>(arr);
+            str.Insert(55, 1);
+            str.Remove(55);
+            Console.ReadKey();
+        }
+    }
+}
