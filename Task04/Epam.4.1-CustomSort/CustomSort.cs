4a8785cf2d1de60acf3e2376819475f85bea764e
Maksim Leontev <akagrand1k@yandex.ru>
2017-03-12 22:22:04 +0300

    Task 04 add

---
+++ Task04/Epam.4.1-CustomSort/CustomSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Epam._4._1_CustomSort
+{
+    public static class CustomSort
+    {
+        /// <summary>
+        /// Sorting generic arrays.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_array">Generic Array</param>
+        /// <param name="TypeSort">Sort Conditions</param>
+        public static void Sort<T>(T[] _array, Func<T, T, bool> TypeSort)
+        {
+            if (TypeSort!= null)
+            {
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    for (int j = 0; j < _array.Length - 1; j++)
+                    {
+                        if (TypeSort(_array[j], _array[j + 1]))
+                            Swap(ref _array[j], ref _array[j + 1]);
+                    }
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Swap generic element.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
